914a580461b86c5ce732974eeda972bb5d2c4f21
Imię i nazwiskoPiotr <raszta9999@wp.pl>
2018-10-02 18:48:34 +0200

    Adding slider js

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
 
     const playVideo = document.querySelector('.video__container');
-    
+    const sliderItem = document.querySelectorAll('.slider__item');
+    const dotsSlider = document.querySelectorAll('.dot__el');
+
     playVideo.addEventListener('click', function(e){
         const video = document.querySelector('video');
        
@@ -11,7 +13,19 @@ document.addEventListener('DOMContentLoaded', function(){
             video.pause();
         }
     });
+    
+    let slideIndex = 0;
+    showSlides();
 
-
-
+    function showSlides() {
+        sliderItem.forEach(el => el.style.display = "none");
+        dotsSlider.forEach(el => el.classList.remove('active'));
+        slideIndex++;
+        if (slideIndex > sliderItem.length) {
+             slideIndex = 1; 
+        }
+        sliderItem[slideIndex - 1].style.display = "block";
+        dotsSlider[slideIndex - 1].classList.add('active');
+        setTimeout(showSlides, 3000); 
+    }
 });
